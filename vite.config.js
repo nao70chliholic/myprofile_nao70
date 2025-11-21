ce819cf7267d11e897fcfc85cde6893a64be5caf
nao70chliholic <nashi70nero@gmail.com>
2025-11-21 16:14:36 +0900

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/myprofile_nao70/' : '/',
+  base: '/myprofile_nao70/',
   plugins: [react()],
 })
